4b250cd208364a4abcdbb421983ae5e32e08944c
dandlestoregroup-cyber <dandlestoregroup@gmail.com>
2026-08-23 18:58:51 +0300

    feat: focus homepage on North Coast campaign journey

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,31 +1,34 @@
+import { useEffect } from "react";
 import Navigation from "@/components/Navigation";
 import TopBanner from "@/components/TopBanner";
 import Hero from "@/components/Hero";
-import Quote from "@/components/Quote";
 import PromiseGrid from "@/components/PromiseGrid";
 import ARDemo from "@/components/ARDemo";
 import ProductGallery from "@/components/ProductGallery";
-import Partners from "@/components/Partners";
 import SocialProof from "@/components/SocialProof";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import WhatsAppFloat from "@/components/WhatsAppFloat";
+import NorthCoastConsultation from "@/components/north-coast/NorthCoastConsultation";
+import { captureCampaignAttribution, trackCampaign } from "@/lib/campaign";
 
 const Index = () => {
+  useEffect(() => {
+    captureCampaignAttribution();
+    trackCampaign("north_coast_view", { surface: "homepage" });
+  }, []);
+
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background">
       <TopBanner />
       <Navigation />
       <main>
         <Hero />
-        <Quote />
         <PromiseGrid />
+        <NorthCoastConsultation surface="module" />
         <ARDemo />
         <div id="products">
           <ProductGallery />
-        </div>
-        <div id="partners">
-          <Partners />
         </div>
         <SocialProof />
         <Contact />
